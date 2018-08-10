5dd5236702a69fb013fcba0ae3389832b07b33c2
wz2cool <wz2cool@live.cn>
2018-08-10 22:39:52 +0800

    add

---
+++ src/model/enum/filterCondition.ts
@@ -0,0 +1,10 @@
+export enum FilterCondition {
+    /**
+     * And filter condition.
+     */
+    AND,
+    /**
+     * Or filter condition.
+     */
+    OR,
+}
---
+++ src/model/enum/filterOperator.ts
@@ -0,0 +1,50 @@
+export enum FilterOperator {
+    /**
+     * Less than filter operator.
+     */
+    LESS_THAN,
+    /**
+     * Less than or equal filter operator.
+     */
+    LESS_THAN_OR_EQUAL,
+    /**
+     * Equal filter operator.
+     */
+    EQUAL,
+    /**
+     * Not equal filter operator.
+     */
+    NOT_EQUAL,
+    /**
+     * Greater than or equal filter operator.
+     */
+    GREATER_THAN_OR_EQUAL,
+    /**
+     * Greater than filter operator.
+     */
+    GREATER_THAN,
+    /**
+     * Start with filter operator.
+     */
+    START_WITH,
+    /**
+     * End with filter operator.
+     */
+    END_WITH,
+    /**
+     * Contains filter operator.
+     */
+    CONTAINS,
+    /**
+     * In filter operator.
+     */
+    IN,
+    /**
+     * Not in filter operator.
+     */
+    NOT_IN,
+    /**
+     * Between filter operator.
+     */
+    BETWEEN,
+}
