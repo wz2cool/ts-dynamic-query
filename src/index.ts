c8646370219c9a4f5bbecd8955fefbb86a83828d
wz2cool <wz2cool@live.cn>
2018-09-01 11:10:12 +0800

    update 1.0.21

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { FilterCondition } from "../src/enums/filterCondition";
-import { FilterOperator } from "../src/enums/filterOperator";
-import { SortDirection } from "../src/enums/sortDirection";
-import { SortHelper } from "../src/helpers/sortHelper";
-import { FilterHelper } from "../src/helpers/filterHelper";
-import { DynamicQuery } from "../src/models/dynamicQuery";
-import { FilterDescriptor } from "../src/models/filterDescriptor";
-import { FilterDescriptorBase } from "../src/models/filterDescriptorBase";
-import { FilterGroupDescriptor } from "../src/models/filterGroupDescriptor";
-import { FilterGroupOptions } from "../src/models/filterGroupOptions";
-import { FilterOptions } from "../src/models/filterOptions";
-import { PredicteResult } from "../src/models/predicteResult";
-import { SortDescriptor } from "../src/models/sortDescriptor";
-import { SortDescriptorBase } from "../src/models/sortDescriptorBase";
-import { SortOptions } from "../src/models/sortOptions";
-import { QueryProvider } from "../src/providers/queryProvider";
+import { FilterCondition } from "./enums/filterCondition";
+import { FilterOperator } from "./enums/filterOperator";
+import { SortDirection } from "./enums/sortDirection";
+import { SortHelper } from "./helpers/sortHelper";
+import { FilterHelper } from "./helpers/filterHelper";
+import { DynamicQuery } from "./models/dynamicQuery";
+import { FilterDescriptor } from "./models/filterDescriptor";
+import { FilterDescriptorBase } from "./models/filterDescriptorBase";
+import { FilterGroupDescriptor } from "./models/filterGroupDescriptor";
+import { FilterGroupOptions } from "./models/filterGroupOptions";
+import { FilterOptions } from "./models/filterOptions";
+import { PredicteResult } from "./models/predicteResult";
+import { SortDescriptor } from "./models/sortDescriptor";
+import { SortDescriptorBase } from "./models/sortDescriptorBase";
+import { SortOptions } from "./models/sortOptions";
+import { QueryProvider } from "./providers/queryProvider";
 
 export {
   FilterCondition,
