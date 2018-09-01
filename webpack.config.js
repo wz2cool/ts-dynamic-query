173cb79898d3cf1eae07ff7a7a5bad07ecd702c2
wz2cool <wz2cool@live.cn>
2018-09-01 10:26:34 +0800

    add

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+const config = {
+  mode: "production",
+  entry: {
+    index: "./src/index.ts",
+    "index.min": "./src/index.ts"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    globalObject: "typeof self !== 'undefined' ? self : this",
+    libraryTarget: "umd",
+    library: "ts-dynamic-query",
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  devtool: "source-map",
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        include: /\.min\.js$/
+      })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loaders: ["babel-loader", "ts-loader"],
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
