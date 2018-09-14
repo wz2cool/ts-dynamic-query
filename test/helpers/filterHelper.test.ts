2a5b281b5716f1b1b64cf00bc89fd5a5f1a0738d
wz2cool <wz2cool@live.cn>
2018-09-14 16:11:11 +0800

    add start with

--- test/helpers/filterHelper.test.ts
+++ test/helpers/filterHelper.test.ts
@@ -190,4 +190,243 @@ describe(".filterHelper", () => {
       expect(true).to.be.eq(result);
     });
   });
+
+  describe("#predicateLessThanOrEqual", () => {
+    it("'number' should return true if 18 <= 19", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateLessThanOrEqual<Student>(
+        student,
+        "age",
+        19
+      );
+      expect(true).to.be.eq(result);
+    });
+
+    it("'number' should return true if 18 <= 18", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateLessThanOrEqual<Student>(
+        student,
+        "age",
+        18
+      );
+      expect(true).to.be.eq(result);
+    });
+
+    it("'number' should return false if 18 <= 10", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateLessThanOrEqual<Student>(
+        student,
+        "age",
+        10
+      );
+      expect(false).to.be.eq(result);
+    });
+
+    it("should return false if 18 <= 'test'", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateLessThanOrEqual<Student>(
+        student,
+        "age",
+        "test"
+      );
+      expect(false).to.be.eq(result);
+    });
+
+    it("should return true if 'Jack' <= 'test'", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateLessThanOrEqual<Student>(
+        student,
+        "name",
+        "test"
+      );
+      expect(true).to.be.eq(result);
+    });
+  });
+
+  describe("#predicateGreaterThanOrEqual", () => {
+    it("'number' should return false if 18 >= 19", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThanOrEqual<Student>(
+        student,
+        "age",
+        19
+      );
+      expect(false).to.be.eq(result);
+    });
+
+    it("'number' should return true if 18 <= 18", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThanOrEqual<Student>(
+        student,
+        "age",
+        18
+      );
+      expect(true).to.be.eq(result);
+    });
+
+    it("'number' should return true if 18 >= 10", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThanOrEqual<Student>(
+        student,
+        "age",
+        10
+      );
+      expect(true).to.be.eq(result);
+    });
+
+    it("should return false if 18 >= 'test'", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThanOrEqual<Student>(
+        student,
+        "age",
+        "test"
+      );
+      expect(false).to.be.eq(result);
+    });
+
+    it("should return false if 'Jack' >= 'test'", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThanOrEqual<Student>(
+        student,
+        "name",
+        "test"
+      );
+      expect(false).to.be.eq(result);
+    });
+  });
+
+  describe("#predicateGreaterThan", () => {
+    it("'number' should return false if 18 > 19", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThan<Student>(
+        student,
+        "age",
+        19
+      );
+      expect(false).to.be.eq(result);
+    });
+
+    it("'number' should return false if 18 > 18", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThan<Student>(
+        student,
+        "age",
+        18
+      );
+      expect(false).to.be.eq(result);
+    });
+
+    it("'number' should return true if 18 > 10", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThan<Student>(
+        student,
+        "age",
+        10
+      );
+      expect(true).to.be.eq(result);
+    });
+
+    it("should return false if 18 > 'test'", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThan<Student>(
+        student,
+        "age",
+        "test"
+      );
+      expect(false).to.be.eq(result);
+    });
+
+    it("should return false if 'Jack' > 'test'", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateGreaterThan<Student>(
+        student,
+        "name",
+        "test"
+      );
+      expect(false).to.be.eq(result);
+    });
+  });
+
+  describe("#predicateStartWith", () => {
+    it("should return true if 'Jack' start with 'Ja'", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateStartWith(
+        student,
+        "name",
+        "Ja",
+        false
+      );
+      expect(true).to.be.eq(result);
+    });
+
+    it("should return true if 'Jack' start with 'ja' with ignore case", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateStartWith(
+        student,
+        "name",
+        "ja",
+        true
+      );
+      expect(true).to.be.eq(result);
+    });
+
+    it("should return true if 'Jack' start with 'te'", () => {
+      const student = new Student();
+      student.name = "Jack";
+      student.age = 18;
+      const result = FilterHelper.predicateStartWith(
+        student,
+        "name",
+        "te",
+        false
+      );
+      expect(false).to.be.eq(result);
+    });
+
+    it("should return false if propertyValue is null or undefined", () => {
+      const student = new Student();
+      student.age = 18;
+      const result = FilterHelper.predicateStartWith(
+        student,
+        "name",
+        "te",
+        false
+      );
+      expect(false).to.be.eq(result);
+    });
+  });
 });
